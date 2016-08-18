83a97a8ffe585045b4d6976de58f29e2f390377a
Miguel Dos Santos Pereira <miguel@macbook-pro-de-miguel.local>
2016-08-18 13:14:08 -0300

    Initial Commit

---
+++ Algoritmo Genetico/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algoritmo Genetico
+//
+//  Created by Miguel dos Santos Pereira on 18/08/16.
+//  Copyright © 2016 Miguel dos Santos Pereira. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
